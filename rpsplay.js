d1261993e2decbaceff6c59247cbb03e3d96dcd1
cxcuser <ibyam360@gmail.com>
2023-09-30 21:09:58 -0400

    Create an array to hold rock, paper, scissors.
    create an array for choices R, P, C, and generate a random number from 0 to 2 to select from this
    array.

--- rpsplay.js
+++ rpsplay.js
@@ -1,7 +1,16 @@
 
 
 function getComputerChoice() {
-    return "ROCK";
+
+    //create an array for choices of play
+    const playSelect = ["ROCK", "PAPER", "SCISSORS"];
+    
+
+    //generate a randon number between 1 and 3, inclusive.
+    const num = Math.floor(Math.random() * 100)%3;
+
+    return [playSelect[num], num];
+
 }
 
 const output = getComputerChoice();
