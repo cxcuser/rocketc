b48d9e30a3ac089af9ccb8176bfb13beb6f9b426
cxcuser <ibyam360@gmail.com>
2023-09-30 21:57:09 -0400

    Adjust code to reflect structure of item 5 of the assignment statement.
    Adjust the code to reflect the item 5 structure, but without any comparison logic as yet.

--- rpsplay.js
+++ rpsplay.js
@@ -3,20 +3,25 @@
 function getComputerChoice() {
 
     //create an array for choices of play
-    const playSelect = ["ROCK", "PAPER", "SCISSORS"];
+    let playSelect = ["ROCK", "PAPER", "SCISSORS"];
     
 
     //generate a randon number between 1 and 3, inclusive.
     const num = Math.floor(Math.random() * 100)%3;
 
-    //prompt user
-    userPlay = prompt();
-    computerPlay = playSelect[num];
+    
+    let computerPlay = playSelect[num];
 
-    return [computerPlay, num, userPlay ];
+    return computerPlay;
 
 }
 
-const output = getComputerChoice();
+function playRound(userPlay, computerPlay) {
+   return [userPlay, computerPlay]
+}
 
-console.log(output);
+const computerPlay = getComputerChoice();
+//prompt user
+const userPlay = prompt().toUpperCase();
+
+console.log(playRound(userPlay, computerPlay) );
