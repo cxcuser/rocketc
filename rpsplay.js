b37e0dc62752ba83bc37f62fd08ad2cf92aabd4b
cxcuser <ibyam360@gmail.com>
2023-10-01 21:00:45 -0400

    Move comparison logic into playRound functiom
    Move the comparison logic into the playRound function and test again.

--- rpsplay.js
+++ rpsplay.js
@@ -5,44 +5,42 @@ function getComputerChoice() {
     //create an array for choices of play
     let playSelect = ["ROCK", "PAPER", "SCISSORS"];
     
-
     //generate a randon number between 1 and 3, inclusive.
     const num = Math.floor(Math.random() * 100)%3;
-
     
     let computerPlay = playSelect[num];
     
-
     return computerPlay;
-
 }
 
 function playRound(userPlay, computerPlay) {
-   return [userPlay, computerPlay]
+    
+    let gameResult = "DRAW!";
+
+    if (userPlay === "ROCK") {
+        if (computerPlay === "SCISSORS")
+            gameResult = "User WINS!";
+        else if (computerPlay === "PAPER") 
+            gameResult = "Computer WINS!";
+    }
+    if (userPlay === "PAPER") {
+        if (computerPlay === "SCISSORS")
+            gameResult = "Computer WINS!";
+        else if (computerPlay === "ROCK") 
+            gameResult = "User WINS!";
+    }
+    if (userPlay === "SCISSORS") {
+        if (computerPlay === "ROCK")
+            gameResult = "Computer WINS!";
+        else if (computerPlay === "PAPER") 
+            gameResult = "User WINS!";
+    }
+    return [userPlay, computerPlay, gameResult]
 }
 
+let userPlay = "ROCK";
 const computerPlay = getComputerChoice();
 //prompt user
-const userPlay = prompt().toUpperCase();
-let gameResult = "DRAW!";
+//const userPlay = prompt().toUpperCase();
 
-if (userPlay === "ROCK") {
-    if (computerPlay === "SCISSORS")
-        gameResult = "User WINS!";
-    else if (computerPlay === "PAPER") 
-        gameResult = "Computer WINS!";
-}
-if (userPlay === "PAPER") {
-    if (computerPlay === "SCISSORS")
-        gameResult = "Computer WINS!";
-    else if (computerPlay === "ROCK") 
-        gameResult = "User WINS!";
-}
-if (userPlay === "SCISSORS") {
-    if (computerPlay === "ROCK")
-        gameResult = "Computer WINS!";
-    else if (computerPlay === "PAPER") 
-        gameResult = "User WINS!";
-}
-
-console.log([...playRound(userPlay, computerPlay), gameResult]);
+console.log(playRound(userPlay, computerPlay));
